e91fb23882297f15f97b25dfd8b5bbdaa0f5f984
Kaushik Kumar Barman <kaushik.kb4@gmail.com>
2021-04-20 12:20:05 +0530

    created app using create-react-app and added menu items as dummy

---
+++ src/components/menu-item/menu-item.component.jsx
@@ -0,0 +1,15 @@
+import './menu-item.styles.css';
+
+const MenuItem = ({ title, imageUrl, size }) => (
+    <div style={{
+        backgroundImage: `url(${imageUrl})`
+    }}
+    className={`menu-item ${size}`}>
+        <div className="content">
+            <h1 className="title">{title}</h1>
+            <span className="subtitle">SHOP NOW</span>
+        </div>
+    </div>
+);
+
+export default MenuItem;
