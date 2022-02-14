19761eba01849536f3ce9bbb154a61f7663e3263
Kaushik Kumar Barman <kaushik.kb4@gmail.com>
2022-02-14 07:08:28 +0530

    updated cart items to group similar items

--- src/redux/cart/cart.reducer.js
+++ src/redux/cart/cart.reducer.js
@@ -1,13 +1,20 @@
 import CartActionTypes from './cart.types';
+import { addItemToCart } from './cart.utils';
 
 const INITIAL_STATE = {
-    hidden: true
+    hidden: true,
+    cart: []
 }
 
 const cartReducer = (state = INITIAL_STATE, action) => {
     switch(action.type) {
         case CartActionTypes.TOGGLE_CART_DROPDOWN:
-            return { hidden: !state.hidden }
+            return { ...state, hidden: !state.hidden }
+        case CartActionTypes.ADD_ITEM:
+            return {
+                ...state,
+                cart: addItemToCart(state.cart, action.payload)
+            }
         default: 
             return state;
     }
