984f49f8bf033232f116a0bfc84a96c08c7287cf
Kaushik Kumar Barman <kaushik.kb4@gmail.com>
2022-02-28 02:39:40 +0530

    added clear cart sagas on sign out success

--- src/redux/shop/shop.sagas.js
+++ src/redux/shop/shop.sagas.js
@@ -1,4 +1,4 @@
-import { takeEvery, call, put } from 'redux-saga/effects';
+import { takeEvery, call, put, all } from 'redux-saga/effects';
 import { firestore } from '../../firebase/firebase.utils';
 
 import ShopActionTypes from './shop.types';
@@ -18,4 +18,8 @@ export function* fetchCollectionsAsync() {
 
 export function* fetchCollectionsStart() {
     yield takeEvery(ShopActionTypes.FETCH_COLLECTIONS_START, fetchCollectionsAsync);
+}
+
+export function* shopSagas() {
+    yield all([call(fetchCollectionsStart)])
 }
