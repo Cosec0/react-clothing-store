278a9a1ea8b316f4ce66f46a46b10f6895295137
Kaushik Kumar Barman <kaushik.kb4@gmail.com>
2022-03-05 21:06:53 +0530

    lazy loaded homepage and sign-in-sign-up-page

--- src/App.js
+++ src/App.js
@@ -1,16 +1,13 @@
-import { useEffect } from 'react';
+import { useEffect, lazy, Suspense } from 'react';
 import { Routes, Route, Navigate } from 'react-router-dom';
 
 import { connect } from 'react-redux';
 import { createStructuredSelector } from 'reselect';
 
-// import './App.css';
 import { GlobalStyle } from './global.styles';
 
-import HomePage from './pages/homepage/homepage.component';
 import ShopPage from './pages/shop/shop.component';
 import Checkout from './pages/checkout/checkout.component';
-import SignInSignUpPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
 
 import CollectionsOverviewContainer from './components/collections-overview/collection-overview.container';
 import CollectionPageContainer from './pages/collection/collection.container';
@@ -20,6 +17,10 @@ import Header from './components/header/header.component';
 import { selectCurrentUser } from './redux/user/user.selector';
 
 import { checkUserSession } from './redux/user/user.action';
+import Spinner from './components/spinner/spinner.component';
+
+const HomePage = lazy(() => import('./pages/homepage/homepage.component'));
+const SignInSignUpPage = lazy(() => import('./pages/sign-in-and-sign-up/sign-in-and-sign-up.component'));
 
 
 const App = ({ currentUser, checkUserSession }) => {
@@ -32,7 +33,11 @@ const App = ({ currentUser, checkUserSession }) => {
       <GlobalStyle />
       <Header/>
       <Routes>
-        <Route path='/' element={<HomePage/>} />
+        <Route path='/' element={
+          <Suspense fallback={<Spinner/>}>
+            <HomePage/>
+          </Suspense>
+        } />
         <Route path='/shop' element={<ShopPage/>} >
           <Route path='/shop' element={<CollectionsOverviewContainer/>} />
           <Route path='/shop/:collectionId' element={<CollectionPageContainer/>} />
@@ -40,7 +45,9 @@ const App = ({ currentUser, checkUserSession }) => {
         <Route path='/signIn' element={
           currentUser ? 
           <Navigate replace to='/'/> :
-          <SignInSignUpPage/>
+          <Suspense fallback={<Spinner/>}>
+            <SignInSignUpPage/>
+          </Suspense>
         } />
         <Route path='/checkout' element={<Checkout/>} />
       </Routes>
